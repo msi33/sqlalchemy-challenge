3f7daceb03cbdcd22e634671fc1820628cdd4c7c
msi33 <msomowork@gmail.com>
2024-05-23 12:44:35 -0500

    established database and reflected

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,18 +1,31 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+import datetime as dt
 
 
 #################################################
 # Database Setup
+database_path = "Resources/hawaii.sqlite"
+engine = create_engine(f"sqlite:///{database_path}")
 #################################################
 
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
 
 # Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 
 # Create our session (link) from Python to the DB
