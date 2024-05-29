e42e8df3814aa87e0f291d41eac92e7101f08b07
msi33 <msomowork@gmail.com>
2024-05-28 23:26:03 -0500

    get station and prcp info

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,4 +1,5 @@
 # Import the dependencies.
+from flask import Flask, jsonify
 import numpy as np
 from pathlib import Path
 import sqlalchemy
@@ -6,8 +7,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect, func
 
-# from flask import Flask, jsonify
-# import datetime as dt
+
+import datetime as dt
 
 
 # #################################################
@@ -38,39 +39,76 @@ Station = Base.classes.station
 session = Session(engine)
 
 
-
-
 # #################################################
 # # Flask Setup
 # #################################################
 # from flask import Flask
-
-# # 2. Create an app, being sure to pass __name__
-# app = Flask(__name__)
-
-
-# # 3. Define what to do when a user hits the index route
-# @app.route("/")
-# def welcome():
-#     """List all available api routes"""
-#     return(
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/precipitation<br/>"
-#         f"/api/v1.0/stations<br/>"
-#         f"/api/v1.0/tobs<br/>"
-#         f"/api/v1.0/<start><br/>"
-#         f"/api/v1.0/<start>/<end><br/>"
-#     )
-
-# @app.route("/")
-# def home():
-#     print("Server received request for 'Home' page...")
-#     return "Welcome to my 'Home' page!"
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# # Create an app, being sure to pass __name__
+app = Flask(__name__)
 
 
 # #################################################
 # # Flask Routes
 # #################################################
+
+@app.route("/")
+def homepage():
+    """List all available api routes"""
+    return (
+        f"<h1>Welcome Hawaii climate app</h1><br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+#  Explore: /api/v1.0/precipitation
+
+
+@app.route("/")
+def Precipitation():
+    latest_date = session.query(func.max(Measurement.date))
+    latest_date = dt.datetime.strptime(lastdate, "%Y-%m-%d").date()
+    begin_date = latest_date - dt.timedelta(days=365)
+    begin_date = begin_date.strftime("%Y-%m-%d")
+    over_ayeardata = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date.between(begin_date, latest_date)).all()
+
+    precipts = []
+    for date, precipts in over_ayeardata:
+        precipts_dict = {}
+        precipts_dict['date'] = date
+        precipts_dict['prcp'] = prcp
+        precipts.append(precipts_dict)
+    return jsonify(precipts)
+
+# Explore: /api/v1.0/stations to return JSON list for station
+
+
+@app.route("/")
+def stations():
+
+    station_data = session.query(Station.Name).all()
+    station_data = list(np.ravel(station_data))
+    return jsonify(station_data)
+
+
+# # Explore: /api/v1.0/stations
+# @app.route("/api/v1.0/stations")
+# def stations():
+#     station_data = session.query(Station.station, Station.name).all()
+#     return jsonify(station_data)
+
+# # Explore:/api/v1.0/tobs
+# @app.route("/api/v1.0/tobs")
+# def tobs():
+#     tobs_data = session.query(Measurement.date, Measurement.station, Measurement.tobs).filter(Measurement.date >= last_twelve_months).all()
+#     return jsonify(tobs_data)
+
+ # Date 12 months ago
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
